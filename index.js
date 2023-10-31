97773d0bed2cd3c58012b9a6acf75726ec87dac9
Vance-ng-vn <vance.ng.vn@gmail.com>
2023-10-31 17:15:13 +0700

    Auto-commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Clean project...");
+const express = require('express');
+
+const app = express();
+
+app.get('*', (req, res) => {
+    res.send("Closed");
+});
+
+app.listen(process.env.PORT);
