a53e2f666923294e928dfbc2300c369944790e77
Vance-ng-vn <vance.ng.vn@gmail.com>
2023-11-02 17:50:04 +0700

    Auto-commit

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ app.get('*', (req, res) => {
 // delete process.env.CACHE;
 // delete process.env.NPM_CONFIG_LOGLEVEL;
 
-console.log(process.env);
-console.log("listening PORT: " + process.env.PORT);
-app.listen(process.env.PORT || 5000);
+//console.log(process.env);
+const server = app.listen(process.env.PORT || 5000, ()=>{
+    console.log("listening PORT: " + process.env.PORT);
+});
+
+//shutdown Server after 5s
+setTimeout(() => {
+    server.close(() => {
+        console.log("Shuting down Server...");
+    })
+}, 5000);
